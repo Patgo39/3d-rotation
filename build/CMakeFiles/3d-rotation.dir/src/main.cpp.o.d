9d5e5a3485d14f446ce99ffc433c64eed912649e
Patricio García Ocampo <patgo39@gmail.com>
2026-03-09 19:32:36 -0600

    Primera prueba de ncurses

---
+++ build/CMakeFiles/3d-rotation.dir/src/main.cpp.o.d
@@ -0,0 +1,31 @@
+CMakeFiles/3d-rotation.dir/src/main.cpp.o: \
+ /home/patriciogarcia/Desktop/Proyects/cpp/projects/3d-rotation/src/main.cpp \
+ /usr/include/stdc-predef.h /usr/include/curses.h \
+ /usr/include/ncurses_dll.h \
+ /usr/lib/gcc/x86_64-redhat-linux/15/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-redhat-linux/15/include/stddef.h \
+ /usr/lib/gcc/x86_64-redhat-linux/15/include/stdarg.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h \
+ /usr/lib/gcc/x86_64-redhat-linux/15/include/stdbool.h \
+ /usr/include/wchar.h /usr/include/bits/types/wint_t.h \
+ /usr/include/bits/types/mbstate_t.h /usr/include/bits/types/locale_t.h \
+ /usr/include/bits/types/__locale_t.h /usr/include/unctrl.h \
+ /usr/include/curses.h /usr/include/unistd.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/confname.h /usr/include/bits/getopt_posix.h \
+ /usr/include/bits/getopt_core.h /usr/include/bits/unistd_ext.h \
+ /usr/include/linux/close_range.h
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
-#include <iostream>
+#include <ncurses.h>
+#include <unistd.h> 
 
-int main(int argc, char *argv[]){
+struct Point{
+  int x;
+  int y;
+};
 
-    std::cout<<"Hola Mundo"<<std::endl;
-
-    return 0;
+void printPoint(Point p){
+  mvaddch(p.y, p.x, '&');
 }
+
+void startNcursesScreen(){
+  initscr();             // Inicia el modo ncurses
+  noecho();              // No muestra las teclas que presionas
+  curs_set(0);           // Oculta el cursor físico
+  nodelay(stdscr, TRUE); // Activa no bloqueante
+}
+
+void endNcursesScreen(){
+  printw("\n\n Presiona cualquier tecla...");
+  nodelay(stdscr, FALSE);    // Desactiva no bloqueante
+  getch();                 // 
+  endwin();                // Termina el modo ncurses
+}
+
+
+int main() {
+  startNcursesScreen();
+
+  // 24 80
+  int max_y, max_x;
+  getmaxyx(stdscr, max_y, max_x);
+
+  printw("%d\n", max_y);
+  printw("%d\n",max_x);
+  
+  endNcursesScreen();
+  
+  return 0;
+}
+
