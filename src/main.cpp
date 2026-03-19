0cb9469fd76bb1fd228550771de11ba8ef6fc06a
Patricio García Ocampo <patgo39@gmail.com>
2026-03-18 23:31:28 -0600

    Se factorizó el código en varias funciones

--- src/main.cpp
+++ src/main.cpp
@@ -4,52 +4,86 @@
 #include <signal.h>
 #include <cstdlib>
 
-void exitHandler(int signal){
+// Serán declarados más adelante
+int max_width = 0; 
+int max_height = 0;
+
+/**
+ * Maneja el evento de CTRL + C para interrumpir
+ * la simulación.
+ */
+void exitEventHandler(int signal){
   printf("\033[?25h\033[0m\n");
   exit(0);
 }
 
+/**
+ * Resetea el cursor. Utiliza secuencia de escape ANSI (SCI) para
+ * limpiar el buffer de la terminal (2J) y devolver el cursor a la
+ * posición home (H). 
+ */
 void clearScreen() {
     printf("\033[2J\033[H");
 }
 
-void printCharToXY(double x, double y, char c){
-  printf("\033[%d;%dH#", (int)y, (int)x);
-  fflush(stdout);
+/**
+ * Dibuja un carácter en coordenadas específicas de la terminal.
+ * Se usa la secuencia de escape ANSI 'CUP' para mover el cursor
+ * a y, x antes de imprimir. 
+ */
+void putChar(int x, int y, char c){
+  printf("\033[%d;%dH#", y, x);
+  fflush(stdout); // Se imprime inmediatamente el carácter.
 }
 
-int main() {
-  signal(SIGINT, exitHandler);
-  
+/**
+ * Obtiene los máximos de 
+ */
+void buildEscentials(){
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-
-  double x = w.ws_col / 2.0; 
-  double y = w.ws_row / 2.0; 
-  double dx = 1.0;         
-  double dy = 1.0;          
-  
-    // Ocultar el cursor
+  max_width = w.ws_col; // Se declara el max de X
+  max_height = w.ws_row; // Se declara el max de Y
+  // Ocultar el cursor
   printf("\033[?25l");
+}
+
+/**
+ * Inicia la simulación.
+ * La combianción CTRL + C termina la ejecución. 
+ */
+void startSimulation(){
+  signal(SIGINT, exitEventHandler);
+  
+  int x = max_width / 2; 
+  int y = max_height / 2; 
+  int dx = 1;         
+  int dy = 1;          
   
   while (1) {
     clearScreen();
     
-    printCharToXY(x, y, '#');
+    putChar(x, y, '#');
     
     x += dx;
     y += dy;
     
-    if (x >= w.ws_col || x <= 1) {
+    if (x >= max_width || x <= 1) {
       dx *= -1;
     }
     
-    if (y >= w.ws_row || y <= 1) {
+    if (y >= max_height || y <= 1) {
       dy *= -1;
     }
     
     usleep(50000);
   }
+}
+
+int main() {
   
+  buildEscentials();
+  startSimulation();
+ 
   return 0;
 }
